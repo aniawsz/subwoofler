ee0ef94ea9a57d4d02eb5d5e51204c04af0a6464
Anna Wszeborowska <anna.wszeborowska@gmail.com>
2018-10-07 18:18:01 +0200

    Map keyboard keys to midi notes
    It creates a playable octave (C3-C4)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from getch import getch
+from translations import KEYBOARD_KEY_TO_MIDI_NOTE
 
 
 def run():
@@ -9,6 +10,11 @@ def run():
             exit(0)
         else:
             print("pressed: ", char)
+            try:
+                midi_note = KEYBOARD_KEY_TO_MIDI_NOTE[char]
+                print("midi note: ", midi_note)
+            except KeyError:
+                print("note not supported")
 
 
 if __name__ == '__main__':
