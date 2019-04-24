b2a19f6bc9099a86eb7aa5026a11d352929c3ccd
Anna Wszeborowska <anna.wszeborowska@gmail.com>
2019-04-24 23:59:15 +0200

    Create a window app which spawns an audio thread and stops is on closing

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
+import tkinter as tk
+
 from queue import Queue
 
 from getch import getch
 from rompler import Rompler
 from translations import KEYBOARD_KEY_TO_MIDI_NOTE
+
+
+class Application(tk.Frame):
+    def __init__(self, root, *a, **kw):
+        super(Application, self).__init__(root, *a, **kw)
+
+        # Set up the main window
+        root.title("Subwoofler")
+        root.geometry("600x400+400+150")
+        root.config(bg="#ffffff")
+        root.resizable(0,0)
+
+        self._root = root
+        self._notes_queue = Queue(maxsize=1)
+        self._rompler = Rompler(name="AudioThread", notes_queue=self._notes_queue)
+        self._rompler.start()
+
+        # Stop the audio thread when the app is closing
+        root.protocol("WM_DELETE_WINDOW", self._on_closing)
+
+    def _on_closing(self):
+        self._rompler.stop.set()
+        self._root.destroy()
 
 
 def run():
@@ -25,4 +50,6 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    root = tk.Tk()
+    app = Application(root)
+    app.mainloop()
